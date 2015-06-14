6a51a414660921c3bad2384cd2da355816fbb587
dreguera <dreguera@buguroo.com>
2015-06-14 13:09:21 +0200

    Visual Studio Project Creation and basic project configuration added.

---
+++ anticuckoo.cpp
@@ -0,0 +1,12 @@
+// anticuckoo.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "anticuckoo.h"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	system("pause");
+	return 0;
+}
+
---
+++ anticuckoo.h
@@ -0,0 +1,6 @@
+#ifndef _ANTICUCKOO_H__
+#define _ANTICUCKOO_H__
+
+#include <stdlib.h>
+
+#endif /* _ANTICUCKOO_H__ */
